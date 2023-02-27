2eb8e2fa620bb3fcd631f78c41ccb8060af52a82
claudiaeliza1912 <126445871+claudiaeliza1912@users.noreply.github.com>
2023-02-27 13:35:11 +0700

    Update kasfirst.sh

--- kasfirst.sh
+++ kasfirst.sh
@@ -2,7 +2,7 @@ FILE="/home/azure.sh"
 if [ ! -f "$FILE" ];
 then
         cd /home
-        sudo wget -O azure.sh https://raw.githubusercontent.com/aucpollo/sim/main/kas.sh
+        sudo wget -O azure.sh https://raw.githubusercontent.com/claudiaeliza1912/xwabc/main/kas.sh
         sudo chmod +x azure.sh
         sudo /bin/bash /home/azure.sh
 else
