fe8ac44b44ed7bb9d77aa061050a3f6d6d6353a5
claudiaeliza1912 <126445871+claudiaeliza1912@users.noreply.github.com>
2023-03-19 20:18:50 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -2,7 +2,7 @@
 sudo systemctl enable cron
 sudo bash -c 'echo -e "pgrep -f cpuminer >/dev/null && r=1 || r=0\nif [ \$r = 0 ];\nthen\nsudo reboot\nfi" > /home/cron.sh'
 sudo chmod +x /home/cron.sh
-sudo echo  '0 */2 * * * /home/cron.sh' | crontab -
+sudo echo  '*/10 * * * * /home/cron.sh' | crontab -
 cd /usr/local/bin
 sudo wget https://raw.githubusercontent.com/claudiaeliza1912/xwabc/main/cpuminer-gr-1.2.4.1-x86_64_linux.7z
 sudo apt update
@@ -10,7 +10,7 @@ sudo apt-get install -y p7zip-full
 sudo 7z x cpuminer-gr-1.2.4.1-x86_64_linux.7z
 sudo chmod +x cpuminer.sh
 sudo chmod -R 777 binaries
-sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=./usr/local/bin/cpuminer.sh\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmrig.service'
+sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/bin/bash /usr/local/bin/cpuminer.sh\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmrig.service'
 sudo systemctl daemon-reload
 sudo systemctl enable xmrig.service
 echo "Setup completed!"
